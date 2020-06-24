ae704a7a86ec75b28a3189b510c1f4bed597764b
ejahn98 <48040007+ejahn98@users.noreply.github.com>
2020-06-24 22:16:48 +0900

    start_again

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,23 @@
+let value = 1;
+console.log(value);
+
+
+//논리 연산자
+const i = !(true && false || true && false || !false);
+//!(true && false || true && false || true)
+//!(false || false ||true)
+//!(true)
+//false
+
+//비교 연산자
+const a = 10;
+const b = 10;
+console.log(a <b);
+console.log(a===b)
+
+//문자열 붙이기
+const c = '안녕';
+const d = '하세요';
+console.log(c+d);
+
+//조건문
---
+++ JAVASCRIPT/self_number.js
@@ -0,0 +1,9 @@
+function self (a, b) {
+let c = a + b;
+let d = a*
+return a + b ;
+}
+
+const sum = self(1,2)
+
+console.log(sum);
